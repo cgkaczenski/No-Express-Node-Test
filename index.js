ce0c9be5b017149e541d1524a47a93d4615b129d
cgkaczenski <kzpulaski@gmail.com>
2015-05-01 17:17:43 -0500

    app listens to process.env.PORT

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-var express = require('express');
-var app = express();
+var http = require('http');
+var port = process.env.PORT || 8080;
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(port, function () {
+    console.log('Server running at ' + port);
 });
